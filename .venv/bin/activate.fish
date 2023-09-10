06b599696a45d3807ce5516f8b62ead2bb54388e
andromedalactea <bry3639@gmil.com>
2023-09-10 13:16:41 -0500

    HW2 Ahora el gráfico de ncuerpos funciona

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/clickgreen/celestial_mechanics/.venv"
+set -gx VIRTUAL_ENV "/home/andromeda/celestial_mechanics/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
